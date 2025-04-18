79599bd5824dba8c65da42d796121e279726b21b
gazzaar <gazzarDev@gmail.com>
2025-04-18 16:13:01 +0200

    update query product to get category name

--- src/models/queries.js
+++ src/models/queries.js
@@ -1,6 +1,10 @@
 import pool from './pool.js';
 const getAllProducts = async function () {
-  const { rows } = await pool.query('SELECT * FROM products');
+  const { rows } = await pool.query(`
+    SELECT p.*, c.category_name
+    FROM products p
+    JOIN categories c ON p.category_id = c.category_id
+  `);
   return rows;
 };
 
