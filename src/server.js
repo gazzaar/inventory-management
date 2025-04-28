65907faa469af9871367a57d3582101576fdac19
gazzaar <gazzarDev@gmail.com>
2025-04-28 14:19:42 +0300

    config express to use css

--- src/server.js
+++ src/server.js
@@ -15,6 +15,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/products', productRouter);
